832da097e90da3680d488c4c307d88d20e4a1396
Sierra <sierra.for85@gmail.com>
2023-10-09 21:24:48 -0300

    Done

---
+++ Python/PY-A01.py
@@ -0,0 +1,8 @@
+quantities = int(input("Digite a quantidade de alunos na turma: "))
+accumulator = 0
+
+for i in range(quantities):
+    ages = int(input(f"Digite a idade do {i+1}° aluno: "))
+    accumulator += ages
+
+print(f"A média de idades da sala (total de {quantities} alunos) é de, aproximadamente, {accumulator / quantities} anos.")
